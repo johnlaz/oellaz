9c9a97d44a770c768e56fa7c4eeea0cbc79e2c45
johnlaz <51280623+johnlaz@users.noreply.github.com>
2026-04-28 00:05:19 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE  = 'oel-v9.5';
+const CACHE  = 'oel-v10.0';
 const ASSETS = ['/', '/index.html', '/manifest.json',
                 '/icons/icon.svg', '/icons/icon-192.png', '/icons/icon-512.png'];
 
